8706e3a60736c597ce87787b1ded4d1dd1e1d26b
Ivan Vahonin <vahoninivan1992@yandex.ru>
2023-07-05 01:03:05 +0300

    added header

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Preloader from '../Preloader/Preloader';
+import Header from '../Header/Header';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header/>
+      <Preloader/>
+    </div>
+
+  )
+}
+
+export default App;
---
+++ src/utils/utils.js
@@ -0,0 +1,1 @@
+export const baseUrl = `https://api.mesto.ivan.practicum.nomoreparties.sbs`;
